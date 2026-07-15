f5a6c490c9d87d483aa83a03f0902af3bccbd501
Ver93 <VictorElvisRotman@outlook.com>
2026-07-15 19:33:45 +0200

    Format Changes

--- src/search/legal_move.cpp
+++ src/search/legal_move.cpp
@@ -1,26 +1,25 @@
 #include "legal_move.h"
 
-
 namespace LegalMove {
-    std::vector<Move> generate(State& state){
+    std::vector<Move> generate(State &state) {
         std::vector<Move> moves = MoveGen::generatePseudoMoves(state);
         std::vector<Move> legal;
         Undo undo;
-        for (auto& move : moves)
-        {
+        for (auto &move : moves) {
             if (move.movingType == Const::MT_CASTLE) {
-                if (MoveVal::isKingInCheckBeforeMove(state)) continue;
-                if (!MoveVal::isCastlingPathSafe(move.to, state.threatMap[state.turn ^ 1])) continue;
+                if (MoveVal::isKingInCheckBeforeMove(state))
+                    continue;
+                if (!MoveVal::isCastlingPathSafe(move.to, state.threatMap[state.turn ^ 1]))
+                    continue;
             }
 
             MoveExec::makeMove(state, move, undo);
             MoveExec::switchTurn(state);
             ThreatGen::updateThreats(state);
 
-            if (!MoveVal::isKingInCheckAfterMove(state)) {
+            if (!MoveVal::isKingInCheckAfterMove(state)){
                 legal.emplace_back(move);
             }
-
 
             MoveExec::undoMove(state, undo);
         }
